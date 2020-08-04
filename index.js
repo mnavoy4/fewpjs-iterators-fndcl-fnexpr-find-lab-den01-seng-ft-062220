0019d24886c1df0abd17d9df16ef8d796c9b03e1
mnavoy4 <mnavoy4@gmail.com>
2020-08-04 14:51:25 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let winner = array.find(entry => entry.result === "W")
+  return winner ? winner.year : undefined;
+}
